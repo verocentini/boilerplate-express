23b7dc70fdabec582ab634ce16d8bd520e22526c
verocentini <veronicacentini06@gmail.com>
2025-11-29 20:43:38 -0300

    Integrate dotenv and mongoose for database connection
    Added dotenv for environment variable management and connected to MongoDB using mongoose.

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+});
 
 
 
