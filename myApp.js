32a83000180a6fa9bfe91d2ee7383208653e0e59
verocentini <veronicacentini06@gmail.com>
2025-11-29 20:45:01 -0300

    Fix mongoose connection syntax and add module export

--- myApp.js
+++ myApp.js
@@ -1,13 +1,16 @@
 let express = require('express');
 let app = express();
+
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
+// freeCodeCamp necesita esto:
+module.exports = app;
 
 
 
